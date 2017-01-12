564974afecda93c8262a55ae05fe148abc842067
Thanasis Polychronakis <thanpolas@gmail.com>
2017-01-12 20:13:21 +0200

    stub kafka on tests boot

--- test/lib/tester.lib.js
+++ test/lib/tester.lib.js
@@ -2,10 +2,32 @@
  * @fileOverview Main testing helper lib.
  */
 
-var tester = module.exports = {};
+const kafkaLib = require('@waldo/node-kafka');
+const kafkaStub = require('@waldo/node-kafka-stub')(kafkaLib);
+
+let booted = false;
+
+var testLib = module.exports = {};
+
+testLib.kafkaStub = kafkaStub;
+
+testLib.init = function() {
+  beforeEach(function() {
+    if (booted) {
+      return;
+    }
+    booted = true;
+
+    testLib.kafkaStub.stub();
+  });
+
+  beforeEach(function() {
+    testLib.kafkaStub.reset();
+  });
+};
 
 /** @type {Object} simple logger */
-tester.log = {
+testLib.log = {
   info: function() {
     let args = Array.prototype.splice.call(arguments, 0);
     console.log('INFO:', args.join(' '));
@@ -26,7 +48,7 @@ tester.log = {
  * @param {number} seconds cooldown in seconds.
  * @return {Function} use is beforeEach().
  */
-tester.cooldown = function(seconds) {
+testLib.cooldown = function(seconds) {
   return function(done) {
     setTimeout(done, seconds);
   };
