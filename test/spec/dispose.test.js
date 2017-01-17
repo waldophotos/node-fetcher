68f6ca24818adf915aa267661cf330d3870dce85
Thanasis Polychronakis <thanpolas@gmail.com>
2017-01-17 13:54:20 +0200

    scaffold dispose testing

---
+++ test/spec/dispose.test.js
@@ -0,0 +1,18 @@
+/**
+ * @fileOverview Test dispose method.
+ */
+
+// const chai = require('chai');
+// const expect = chai.expect;
+// const sinon = require('sinon');
+// const Promise = require('bluebird');
+//
+const testLib = require('../lib/tester.lib');
+// const schemaFix = require('../fixtures/schema.fix');
+// const kafkaLib = require('../lib/kafka.lib');
+//
+// const Fetcher = require('../..');
+
+describe('Disposing', function() {
+  testLib.init();
+});
